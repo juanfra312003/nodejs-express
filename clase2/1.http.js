72f08feeae9028185a665b87d39e5a02585d414f
juanfra312003 <juanframireze@javeriana.edu.co>
2024-03-10 21:26:21 -0500

    Feature: Add nodemon and responses with images

--- clase2/1.http.js
+++ clase2/1.http.js
@@ -1,20 +1,31 @@
 const http = require('node:http')
-
+const fs = require('node:fs')
 const desiredPort = process.env.PORT ?? 1234
 
-// Procesamiento de la petición
+// Procesamiento de la petición -> Mejor usarlo como función
 const processRequest = (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
     if (req.url === '/') {
         res.statusCode = 200
-        res.setHeader('Content-Type', 'text/html; charset=utf-8')
         res.end('<h1> Bienvenido a mi página de inicio. jojo </h1>')
+    } else if (req.url === '/imagen-super-bonita.png') {
+        // Se espera una imágen cómo respuesta
+        fs.readFile('./imagen_jc.png', (err, data) => {
+            if (err) {
+                res.statusCode = 500
+                res.end('<h1> 500: Error interno del servidor </h1>')
+            } else {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'image/png')
+                res.end(data)
+            }
+        })
     } else if (req.url === '/contacto') {
         res.statusCode = 200
-        res.setHeader('Content-Type', 'text/html; charset=utf-8')
         res.end('<h1> Página de contacto </h1>')
     } else {
         res.statusCode = 404
-        res.setHeader('Content-Type', 'text/html; charset=utf-8')
         res.end('<h1> 404: Página no encontrada </h1>')
     }
 }
