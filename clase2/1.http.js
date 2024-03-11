f1465d7c98468b0b0bd71463561767030911ea41
juanfra312003 <juanframireze@javeriana.edu.co>
2024-03-10 21:06:38 -0500

    Feature: Managing server requests

--- clase2/1.http.js
+++ clase2/1.http.js
@@ -2,10 +2,24 @@ const http = require('node:http')
 
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req, res) => {
-    console.log('Peticion recibida', req.url)
-    res.end('Hola mundo')
-})
+// Procesamiento de la petición
+const processRequest = (req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end('<h1> Bienvenido a mi página de inicio. jojo </h1>')
+    } else if (req.url === '/contacto') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end('<h1> Página de contacto </h1>')
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end('<h1> 404: Página no encontrada </h1>')
+    }
+}
+
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
     console.log(`Servidor escuchando en http://localhost:${desiredPort}`)
