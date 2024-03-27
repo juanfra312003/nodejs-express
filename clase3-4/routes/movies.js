3b5c3d9388224a2e4d3cc109e201bf97c273a974
juanfra312003 <juanframireze@javeriana.edu.co>
2024-03-27 13:48:30 -0500

    feature: Controllers MVC

--- clase3-4/routes/movies.js
+++ clase3-4/routes/movies.js
@@ -1,57 +1,12 @@
 // Rutas de películas
 import { Router } from 'express'
 import { readJSON } from '../utils.js'
-import { randomUUID } from 'node:crypto'
-import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
-import { MovieModel } from '../models/movie.js'
-const movies = readJSON('./movies.json')
+import { MovieController } from '../controllers/movies.js'
 
 export const moviesRouter = Router()
 
-moviesRouter.get('/', async (req, res) => {
-    const { genre } = req.query
-    const movies = await MovieModel.getAll({ genre })
-    res.json(movies)
-})
-
-moviesRouter.get('/:id', async (req, res) => {
-    const { id } = req.params
-    const movie = await MovieModel.getById(id)
-    if (movie) return res.json(movie)
-
-    // Si no se encuentra la película
-    res.status(404).json({ message: 'Movie not found' })
-})
-
-moviesRouter.post('/', async (req, res) => {
-    const result = validateMovie(req.body)
-
-    // Toma valores de error o success
-    if (result.error) {
-        return res.status(400).json({
-            error: JSON.parse(result.error.message)
-        })
-    }
-
-    const newMovie = await MovieModel.create({ input: result.data })
-
-    movies.push(newMovie)
-
-    // Código de estado de creación de recursos (201)
-    res.status(201).json(newMovie)
-})
-
-moviesRouter.delete('/:id', async (req, res) => {
-    const { id } = req.params
-    const result = await MovieModel.delete({ id })
-
-    if (result) return res.json({ message: 'Movie deleted' })
-
-    res.status(404).json({ message: 'Movie not found' })
-})
-
-moviesRouter.patch('/:id', async (req, res) => {
-    const { id } = req.params
-    const updatedMovie = await MovieModel.update({ id, input: req.body })
-    return res.json(updatedMovie)
-})
+moviesRouter.get('/', MovieController.getAll)
+moviesRouter.get('/:id', MovieController.getById)
+moviesRouter.post('/', MovieController.create)
+moviesRouter.delete('/:id', MovieController.delete)
+moviesRouter.patch('/:id', MovieController.update)
