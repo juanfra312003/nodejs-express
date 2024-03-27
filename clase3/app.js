95f7e91f66bdac06a5e2e3cbd684811cd473e240
juanfra312003 <juanframireze@javeriana.edu.co>
2024-03-27 11:15:28 -0500

    fix: Rest Client issue

--- clase3/app.js
+++ clase3/app.js
@@ -149,5 +149,5 @@ const PORT = process.env.PORT ?? 1234
 const HOST = process.env.HOST ?? "127.0.0.1"
 
 app.listen(PORT, HOST, () => {
-    console.log(`Server running at http://${HOST}:${PORT}/`)
+    console.log(`Server running at http://localhost:${PORT}/`)
 })
