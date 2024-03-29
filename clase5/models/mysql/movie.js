bc1ee67f7493e541af58ba8cc4bedb9e17ae6396
juanfra312003 <juanframireze@javeriana.edu.co>
2024-03-28 20:55:15 -0500

    feature: Import dependencies and configure connection with mysql

--- clase5/models/mysql/movie.js
+++ clase5/models/mysql/movie.js
@@ -1,25 +1,38 @@
 // Conectar a mongo DB (Dos archivos con el mismo contrato)
 import { readJSON } from "../../utils.js"
+import mysql from 'mysql2'
+
+const config = {
+    host: 'localhost',
+    user: 'root',
+    port: 3306,
+    password: 'mypass',
+    database: 'moviesdb'
+}
+
+const connection = mysql.createConnection(config)
+
+
 const movies = readJSON('./movies.json')
 
 export class MovieModel {
     static getAll = async ({ genre }) => {
-        
+
     }
 
     static async getById({ id }) {
-        
+
     }
 
     static async create({ input }) {
-        
+
     }
 
     static async delete({ id }) {
-        
+
     }
 
     static async update({ id, input }) {
-        
+
     }
 }
