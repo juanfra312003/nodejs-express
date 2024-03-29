707aafa87283883bc533e78457585b42d2e5f3e6
juanfra312003 <juanframireze@javeriana.edu.co>
2024-03-28 21:28:38 -0500

    Feature: Get movies from DB using gender

--- clase5/models/mysql/movie.js
+++ clase5/models/mysql/movie.js
@@ -17,11 +17,33 @@ const movies = readJSON('./movies.json')
 
 export class MovieModel {
     static getAll = async ({ genre }) => {
-        const result = await connection.query(
+        if (genre) {
+            const lowerCaseGenre = genre.toLowerCase()
+
+            // Tomar los ids de la BBDD usando los nombres de los géneros
+            const [genreIds] = await connection.query(
+                'SELECT id, name FROM genre WHERE LOWER(name) = ?;', [lowerCaseGenre]
+            )
+
+            // no genre found
+            if (genreIds.length === 0) return []
+
+            // Tomar el id del primer resultado.
+            const [{ id }] = genreIds
+
+            // Tomar las películas que tengan el id del género
+            const [movies] = await connection.query(
+                'SELECT tittle, year, director, duration, poster, rate, BIN_TO_UUID(id) FROM movie JOIN movie_genres ON movie.id = movie_genres.movie_id WHERE movie_genres.genre_id = ?;', [id]
+            )
+
+            console.log(movies)
+        }
+
+
+        const [movies] = await connection.query(
             'SELECT tittle, year, director, duration, poster, rate, BIN_TO_UUID(id) FROM MOVIE;'
         )
-
-        console.log(result)
+        console.log(movies)
     }
 
     static async getById({ id }) {
