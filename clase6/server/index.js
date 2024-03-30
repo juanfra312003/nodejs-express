693bf43f706e86b29a6bb58cb7e5d6f3552005cb
juanfra312003 <juanframireze@javeriana.edu.co>
2024-03-29 19:06:47 -0500

    Feature: Connection and disconnection web socket

--- clase6/server/index.js
+++ clase6/server/index.js
@@ -1,17 +1,30 @@
 import express from 'express'
 import logger from 'morgan'
 import path from 'path'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
 const PORT = process.env.PORT || 3000
 const HOST = process.env.HOST || '127.0.0.1'
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('New connection')
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected')
+    })
+})
+
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + path.sep + 'client' + path.sep + 'index.html')
 })
 
-app.listen(PORT, HOST, () => {
+server.listen(PORT, HOST, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
 })
